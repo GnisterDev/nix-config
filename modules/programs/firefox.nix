31d9393736e8c19d06ccfad6dd8205739cd36b3a
gnister <sscharer@online.no>
2026-06-11 22:53:43 +0200

    fix `firefox` spelling

--- modules/programs/firefox.nix
+++ modules/programs/firefox.nix
@@ -1,11 +1,11 @@
 { inputs, ... }:
 {
-  flake.modules.nixos.forefox = {pkgs, ...}: {
-    environment.systemPackages = [ pkgs.forefox ];
+  flake.modules.nixos.firefox = {pkgs, ...}: {
+    environment.systemPackages = [ pkgs.firefox ];
   };
 
-  flake.modules.homeManager.forefox = { pkgs, ... }: {
-    programs.forefox = {
+  flake.modules.homeManager.firefox = { pkgs, ... }: {
+    programs.firefox = {
       enable = true;
       languagePacks = ["en-GB" "en-US" "nb-NO"];
     };
