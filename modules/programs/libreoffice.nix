2145ceb0dce65ccb66a34b443bae785015758559
gnister <sscharer@online.no>
2026-06-11 23:06:54 +0200

    `libreoffice` config

---
+++ modules/programs/libreoffice.nix
@@ -0,0 +1,13 @@
+{ inputs, ... }:
+{
+  flake.modules.nixos.libreoffice = {pkgs, ...}: {
+    environment.systemPackages = with pkgs [
+      libreoffice
+
+      hunspell
+      hunspellDicts.nb_NO
+      hunspellDicts.nn_NO
+      hunspellDicts.en_US-large
+    ];
+  };
+}
